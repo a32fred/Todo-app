42fa702e8d717792c265c79d4950f89c50c2accd
a32fred <a.32.freeed@gmail.com>
2025-03-27 09:14:29 -0300

    github workflows:v1.1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Gera uma versão estática do site
-  distDir: "out", // Pasta de saída para exportação
-  images: {
-    unoptimized: true, // Necessário para GitHub Pages
-  },
-  basePath: "/Todo-app", // Defina o nome do repositório
-  assetPrefix: "/Todo-app/",
+  output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
